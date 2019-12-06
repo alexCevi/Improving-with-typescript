d69b7587838d8649d348f1fe81a00ce3ab7044cb
Alex <alex.cevicelow@familydirected.com>
2019-12-05 23:14:53 -0500

    finished basics started objects

---
+++ app.ts
@@ -0,0 +1,16 @@
+// object types
+
+// setting the types the the object contains - key value types
+// const person: {
+//     name: string;
+//     age: number;
+// }
+
+//using infered types is better when working with objects
+const person = {
+    name: 'Alex',
+    age: 20
+};
+
+console.log(person.name)
+;
---
+++ basics.ts
@@ -0,0 +1,10 @@
+//core types
+function add(n1: number, n2: number): number{
+    return n1 + n2;
+}
+
+const number1: number = 5;
+const number2: number = 2.6;
+
+const result = add(number1, number2);
+console.log(result);
